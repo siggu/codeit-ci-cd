02425ea24cb1fe785d3771d0246ac9de697137a1
siggu <m0_w_0m@naver.com>
2025-02-04 09:25:41 +0900

    feat: 새로운 push, pr시 커버리지 리포트 업로드 기능 추가

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
   transform: {
     '^.+.(ts|tsx|js|jsx)$': 'babel-jest',
   },
+  collectCoverage: true, // 코드 커버리지 활성화
+  coverageDirectory: 'coverage', // 커버리지 리포트 저장 위치
+  collectCoverageFrom: ['src/**/*.js'], // 특정 폴더의 파일만 분석
+  coverageReporters: ['json', 'lcov', 'text', 'clover'], // Codecov와 호환되는 형식 추가
 };
